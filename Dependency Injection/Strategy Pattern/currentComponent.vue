dc1227ae5cdbff65f1c8901a1e4d9ddfa0b2435b
anarbabashov <anarbabashov.it@gmail.com>
2025-07-02 16:59:05 +0300

    update: added 2 patters

---
+++ Dependency Injection/Strategy Pattern/currentComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <component :is="currentComponent" />
+</template>
+
+<script setup>
+import { computed } from "vue";
+import ComponentOne from "./ComponentOne.vue";
+import ComponentTwo from "./ComponentTwo.vue";
+import ComponentThree from "./ComponentThree.vue";
+
+const props = defineProps({
+  conditionType: String,
+});
+
+const currentComponent = computed(() => {
+  switch (props.conditionType) {
+    case "one":
+      return ComponentOne;
+    case "two":
+      return ComponentTwo;
+    case "three":
+      return ComponentThree;
+    default:
+      return DefaultComponent;
+  }
+});
+</script>
